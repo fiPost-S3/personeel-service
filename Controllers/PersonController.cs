082ab7a30ad3673dccd756a00a994067ee571a4b
JaapvanderMeer <43168682+JeapfromtheLake@users.noreply.github.com>
2021-04-23 14:08:36 +0200

    project added with swagger, person controller data and model

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using personeel_service.Models;
+
+namespace personeel_service.Controllers
+{
+    [Route("api/persons")]
+    [ApiController]
+    public class PersonController : Controller
+    {
+        private static List<Person> persons = new List<Person>()
+        {
+            new Person("1", "Jaap van der Meer", "jaap@jaap.nl"),
+            new Person("2", "Aron Heesakkers", "aron@aron.nl")
+        };
+
+        // GET: api/Persons
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> GetPerson()
+        {
+            return persons;
+        }
+
+        // GET: api/Persons/5
+        [HttpGet("{id}")]
+        public ActionResult<Person> GetPerson(string id)
+        {
+            var person = persons.Find(p => p.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace personeel_service.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Person(string id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+    }
+}
