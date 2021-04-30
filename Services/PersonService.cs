9ae5bdd896eb018af1b3c7cb92f010bc9b72c25b
JaapvanderMeer <43168682+JeapfromtheLake@users.noreply.github.com>
2021-04-30 15:14:19 +0200

    service layer added

---
+++ Services/PersonService.cs
@@ -0,0 +1,34 @@
+﻿using personeel_service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace personeel_service.Services
+{
+    public class PersonService : IPersonService
+    {
+        private static IEnumerable<Person> persons = new List<Person>()
+        {
+            new Person("1", "Jaap van der Meer", "jaap@jaap.nl"),
+            new Person("2", "Aron Heesakkers", "aron@aron.nl")
+        };
+
+        public List<Person> GetAll()
+        {
+            return persons.ToList();
+        }
+
+        public Person GetById(string id)
+        {
+            var person = persons.FirstOrDefault(p => p.Id == id);
+
+            if (person == null)
+            {
+                throw new Exception($"Person with id {id} not found.");
+            }
+
+            return person;
+        }
+    }
+}
