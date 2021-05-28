fb167e951fe38ed0fc6a7f4821c442a3baeef792
Sjors Scholten <scholtensjors22@outlook.com>
2021-05-28 14:11:02 +0200

    #427 api test (#3)
    * update to return result
    
    * Update PersonTest.cs

--- personeel-service.Test/PersonTest.cs
+++ personeel-service.Test/PersonTest.cs
@@ -7,6 +7,7 @@ using personeel_service.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using personeel_service.Helpers;
 
 namespace personeel_service.Test
 {
@@ -14,8 +15,9 @@ namespace personeel_service.Test
     {
         private Mock<IPersonService> serviceMock;
 
-        private Person person;
-        private List<Person> persons;
+        private Person person1;
+        private Person person2;
+        private readonly List<Person> persons = new List<Person>();
 
         [SetUp]
         public void Setup()
@@ -23,45 +25,79 @@ namespace personeel_service.Test
             // Instantiate mocks:
             serviceMock = new Mock<IPersonService>();
 
-
             // Create mock data:
-            person = new Person("1", "test", "test@niettest.nl");
-            persons = new List<Person>
-            {
-                person,
-                new Person("2", "alf", "test@test.nl"),
-            };
+            person1 = new Person("1", "Person_1", "person_1@testmail.com");
+            person2 = new Person("2", "Person_2", "person_2@testmail.com");
+            
+            persons.Clear();
+            persons.Add(person1);
+            persons.Add(person2);
+        }
+        
+        private static T GetObjectResultContent<T>(ActionResult<T> result)
+        {
+            return (T) ((ObjectResult) result.Result).Value;
         }
 
         [Test]
-        public void GetPersons()
+        public void GetPersons_Ok()
         {
             // Arrange
             serviceMock.Setup(x => x.GetAll()).Returns(persons);
             var controller = new PersonController(serviceMock.Object);
 
             // Act
-            ActionResult<IEnumerable<Person>> result = controller.GetPerson();
+            var actionResult = controller.GetPerson();
 
             // Assert
-            Assert.AreEqual(2, result.Value.ToList().Count);
-            Assert.IsInstanceOf<ActionResult<IEnumerable<Person>>>(result);
+            Assert.IsNotNull(actionResult);
+            Assert.IsInstanceOf<OkObjectResult>(actionResult.Result);
+            Assert.AreEqual(persons, GetObjectResultContent(actionResult));
         }
 
 
         [Test]
-        public void GetPersonById()
+        public void GetPersonById_Ok()
         {
             // Arrange
-            serviceMock.Setup(x => x.GetById("1")).Returns(person);
+            serviceMock.Setup(x => x.GetById(person1.Id)).Returns(person1);
             var controller = new PersonController(serviceMock.Object);
 
             // Act
-            ActionResult<Person> result = controller.GetPerson("1");
+            var actionResult = controller.GetPerson(person1.Id);
 
             // Assert
-            Assert.AreEqual("test", result.Value.Name);
-            Assert.IsInstanceOf<ActionResult<Person>>(result);
+            Assert.IsNotNull(actionResult);
+            Assert.IsInstanceOf<OkObjectResult>(actionResult.Result);
+            Assert.AreEqual(person1, GetObjectResultContent(actionResult));
+        }
+        
+        [Test]
+        public void GetPersonById_NotFound()
+        {
+            // Arrange
+            serviceMock.Setup(x => x.GetById(person1.Id)).Throws<NotFoundException>();
+            var controller = new PersonController(serviceMock.Object);
+
+            // Act
+            var actionResult = controller.GetPerson(person1.Id);
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.IsInstanceOf<NotFoundObjectResult>(actionResult.Result);
+        }
+
+        [Test]
+        public void Health() 
+        {
+            var controller = new PersonController(serviceMock.Object);
+
+            // Act
+            var actionResult = controller.Health();
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.IsInstanceOf<OkResult>(actionResult);
         }
     }
 }
